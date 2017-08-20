99352cecb36a00e3fc5c846b9a64b8fbfd326bc3
Alexander Lutsai <s.lyra@ya.ru>
2017-08-20 23:36:16 +0300

    added scroll functionality to text_field

--- MakiseGUI/gui/elements/inc/makise_e_text_field.h
+++ MakiseGUI/gui/elements/inc/makise_e_text_field.h
@@ -11,21 +11,50 @@ extern "C" {
 
 #include "makise_e.h"
 
+typedef struct {
+    const MakiseFont*       font;
+    uint32_t                font_line_spacing;
+
+    //scroll
+    uint8_t                 scroll_width; //if == 0 then total line count won't be calculated
+    uint32_t                scroll_bg_color; //may be 0
+    uint32_t                scroll_color;
+
+    
+    MakiseStyleTheme  normal;
+    MakiseStyleTheme  focused;
+    MakiseStyleTheme  active;
+} MakiseStyle_TextField;
+
+    
 // This one will display multiline text with multiline, folding and cutting support.
 typedef struct {
-    MakiseGUI*      gui;
-    MElement        el;
+    MakiseGUI*            gui;
+    MElement              el;
 
-    char*           text;
+    char*                 text;
+    uint32_t              len;
 
-    MakiseStyle*    style;
+    uint8_t               enable_scroll;
+    uint32_t              current_line;
+    uint32_t              total_lines ;
+
+    uint8_t                state; //focus state
+    MakiseStyle_TextField* style;
 } MTextField;
 
 void m_create_text_field( MTextField*   b,
                           MContainer*   c,
                           MPosition     pos,
                           char*         text,
-                          MakiseStyle*  style );
+			  uint32_t      len,
+			  uint8_t       enable_scroll,
+                          MakiseStyle_TextField*  style );
+
+void m_text_field_set_text ( MTextField*   b,
+			     char*         text,
+			     uint32_t      len );
+			     
 
 #ifdef __cplusplus
 }
